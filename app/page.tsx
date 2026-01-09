d9eca981d8071e0544618866a672de0a8b3a0fc8
Junaid Abbas <jjbhatti700@gmail.com>
2026-01-09 08:15:58 +0500

    Restore auth.ts file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "./lib/auth";
 import DatabaseBoard from "./components/DatabaseBoard";
 
 export default async function Home() {
-  const session = await getServerSession();
+  const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/login");
